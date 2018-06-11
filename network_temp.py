a033c92a802d39f26d21dbbd606f019847d78c3f
zhouwentong <zhouwentong1993@gmail.com>
2018-06-11 23:10:39 +0800

    Initial commit

---
+++ network_temp.py
@@ -0,0 +1,7 @@
+# wang luo daikaun jisuan
+#print(100/8)
+bandwithdth=100
+ratio=8
+print(bandwithdth/ratio)
+Bandwidth
+
---
+++ zodiac.py
@@ -0,0 +1,14 @@
+zodiac_name = (u'摩羯座', u'水瓶座', u'双鱼座', u'白羊座', u'金牛座', u'双子座', u'巨蟹座',
+               u'狮子座', u'处女座', u'天秤座', u'天蝎座', u'射手座')
+zodiac_days = (
+    (1, 20), (2, 19), (3, 21), (4, 21), (5, 21), (6, 22), (7, 23), (8, 23), (9, 23), (10, 23), (11, 23), (12, 23))
+
+(month, day) = (12, 25)
+zodiac_day = filter(lambda x: x <= (month, day), zodiac_days)
+# print(zodiac_day)
+# print(list(zodiac_day))
+a = len(list(zodiac_day))
+print(a)
+# zodac_len = len(list(zodiac_day)) % 12
+# print(zodac_len)
+# print(zodiac_name[zodac_len])
