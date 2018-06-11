6b9cd4a23073a0a585435a8495996fad5f2491e3
zhouwentong <zhouwentong1993@gmail.com>
2018-06-11 23:49:10 +0800

    做完第四章序列第一部分题目，并学会使用转义字符

---
+++ execrise/execrise04/demo.py
@@ -0,0 +1,16 @@
+# 1. 定义一个字符串Hello Python 并使用print( )输出
+a = 'Hello Python'
+print(a)
+# 2. 定义第二个字符串Let‘s go并使用print( )输出
+b = "Let's go"
+print(b)
+# 3. 定义第三个字符串"The Zen' of Python" -- by Tim Peters 并使用print( )输出
+c = '"The Zen'
+f = "'"
+e = 'of Pytuon"--by Tim Peters'
+print(c+f+e)
+g = ' Let\'s go!\\ta'
+print(g)
+j = '"The Zen\' of Python" -- by Tim Peters'
+#转义字符‘\’：用于单双引号同时存在，无法匹配的情况;;;\转义字符：消除字符原有特殊含义或功能，将其转化成可输出的普通字符
+print(j)
